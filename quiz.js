55694a8c68efe62f991e95771b8149ef31787c6a
MohonSaha <practicewithmohon@gmail.com>
2022-06-13 13:29:05 +0600

    learn local storage

---
+++ quiz.js
@@ -0,0 +1,18 @@
+const array = [];
+
+// const check = array.map();
+// console.log(check);
+
+const name = 'Hero';
+// const age = 34;
+// const person = {name, age}; console.log(person);
+
+let person =null;
+// console.log(person ? person : "person is null");
+
+const obj = {a:1};
+// console.log(Object.keys(obj).length===0);  // Because length is 1
+
+const age= "33";
+const ageNew = ++age;
+console.log(ageNew);
